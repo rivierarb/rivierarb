58320e4dc79124394c30ef73f0fe89bf1f4446d2
Muriel Salvan <Muriel@X-Aeon.com>
2013-04-10 17:31:13 +0200

    Added last presentation, hosted directly on rivierarb.fr

---
+++ content/presentations/_posts/2013-04-02-MessagingSystems.md
@@ -0,0 +1,5 @@
+---
+title: Messaging Systems
+href: http://rivierarb.fr/presentations/messaging-systems
+author: {name: Adina Mihailescu, href: "http://twitter.com/acm1107"}
+---
---
+++ presentations/messaging-systems/slide_config.js
@@ -0,0 +1,33 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'Messaging Systems<br>',
+    subtitle: 'How to make the right choice?',
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
+    favIcon: 'images/MQ_icon.png',
+    fonts: [
+      'Open Calibri:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ],
+    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Adina MIHAILESCU',
+    company: '@acm1107',
+    twitter: '@acm1107',
+    }/*, {
+    name: 'Second Name',
+    company: 'Job Title, Google',
+    gplus: 'http://plus.google.com/1234567890',
+    twitter: '@yourhandle',
+    www: 'http://www.you.com',
+    github: 'http://github.com/you'
+  }*/]
+};
+
